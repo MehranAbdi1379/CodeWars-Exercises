ee2e5f5edebb19e27475b49031ab55420f76a7c6
Mehran Abdi <mehranabdi13793@gmail.com>
2023-09-02 10:43:02 +0330

    Create StripComments method and make it work

--- Strip Comments/Program.cs
+++ Strip Comments/Program.cs
@@ -1,12 +1,26 @@
-﻿internal class Program
+﻿using System.Text.RegularExpressions;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine(StripComments("Apples #this that those !bracelets" , new string[] { "#" , "!"}));
+        Console.WriteLine(StripComments("\n \n   \n \n a #b\nc      \nd $e f g", new string[] { "#" }));
     }
 
     public static string StripComments(string text, string[] commentSymbols)
     {
-        return "";
+        text += "\n";
+        foreach (var symbol in commentSymbols)
+        {
+            var reg = new Regex($"{symbol}.*");
+            if (symbol == "$")
+                reg = new Regex("\\$.*");
+            text = reg.Replace(text, "");
+            var regEx = new Regex("\\s*\n");
+            text = regEx.Replace(text, "\n");
+            text = text.Replace(symbol, "");
+        }
+                text = text.Remove(text.Length - 1, 1);
+        return text;
     }
 }
