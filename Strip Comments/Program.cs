445d22f9f534fd31b8fb9711ab2fe12b46037fbd
Mehran Abdi <mehranabdi13793@gmail.com>
2023-09-02 07:45:45 +0330

    Add project files.

---
+++ Strip Comments/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(StripComments("Apples #this that those !bracelets" , new string[] { "#" , "!"}));
+    }
+
+    public static string StripComments(string text, string[] commentSymbols)
+    {
+        return "";
+    }
+}
