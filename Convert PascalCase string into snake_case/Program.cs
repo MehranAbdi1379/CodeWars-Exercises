3b731da3b4289663e3bb31df3df441b3c233e6a9
Mehran Abdi <mehranabdi13793@gmail.com>
2023-09-25 16:38:18 +0330

    Solve ConvertPascalCaseString problem

---
+++ Convert PascalCase string into snake_case/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.RegularExpressions;
+
+Console.WriteLine(ToUnderscore("TestController98This"));
+
+static string ToUnderscore(string str)
+{
+    var result = str[0].ToString();
+	for (int i = 1; i < str.Length; i++)
+	{
+		var isNumber = int.TryParse(str[i].ToString(), out int a);
+		if (Char.IsUpper(str[i])&& !isNumber)
+			result += $"_";
+		result += str[i];
+	}
+	return result.ToLower();
+}
