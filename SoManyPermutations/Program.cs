47583ad377acfc25857ac73b689ab4eff79d5e19
Mehran Abdi <mehranabdi13793@gmail.com>
2023-10-09 09:36:17 +0330

    write the reccursive method for soManyPermutations

--- SoManyPermutations/Program.cs
+++ SoManyPermutations/Program.cs
@@ -1,13 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-var input = SinglePermutations("abc");
 
-foreach (var item in input)
-{
-    Console.WriteLine(item);
-}
+using System.Diagnostics;
 
+var stopWatch = new Stopwatch();
+stopWatch.Start();
+var input = SinglePermutations("abcfddssdfd");
+stopWatch.Stop();
 
+Console.WriteLine(stopWatch.ElapsedMilliseconds);
 
 
 
@@ -17,18 +18,33 @@ static List<string> SinglePermutations(string s)
     List<char> elements = s.ToList();
     var result = "";
     Reccursive(permutations, elements, result);
+    permutations.Distinct();
     return permutations;
 }
 
-static void Reccursive(List<string> permutations , List<char> elements, string result)
+static void Reccursive(List<string> permutations , List<char> elements,string result)
 {
-    result += elements[0];
-    elements.Remove(elements[0]);
-    if(elements.Count > 0 )
-        Reccursive(permutations,elements, result);
-    else
+    for (int i = 0; i < elements.Count; i++)
     {
-        permutations.Add(result);
+        result += elements[i];
+        var tempElements = new List<char>();
+
+        foreach (var element in elements)
+        {
+            tempElements.Add(element);
+        }
+
+        tempElements.Remove(tempElements[i]);
+
+        if (tempElements.Count > 0)
+        {
+            Reccursive(permutations, tempElements, result);
+            result = result.Remove(result.Length-1,1);
+        }
+        else
+        {
+            permutations.Add(result);
+
+        }
     }
-    
 }
