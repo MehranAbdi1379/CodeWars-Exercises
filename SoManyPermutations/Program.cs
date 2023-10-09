fb50715a27a8497f180d93c1be48141c8070c325
Mehran Abdi <mehranabdi13793@gmail.com>
2023-10-09 08:55:25 +0330

    Write main code for so many permutations project

---
+++ SoManyPermutations/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var input = SinglePermutations("abc");
+
+foreach (var item in input)
+{
+    Console.WriteLine(item);
+}
+
+
+
+
+
+static List<string> SinglePermutations(string s)
+{
+    var permutations = new List<string>();
+    List<char> elements = s.ToList();
+    var result = "";
+    Reccursive(permutations, elements, result);
+    return permutations;
+}
+
+static void Reccursive(List<string> permutations , List<char> elements, string result)
+{
+    result += elements[0];
+    elements.Remove(elements[0]);
+    if(elements.Count > 0 )
+        Reccursive(permutations,elements, result);
+    else
+    {
+        permutations.Add(result);
+    }
+    
+}
